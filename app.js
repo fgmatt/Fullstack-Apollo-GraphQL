19286e0d77d04dc5bb712d19656e93a6e2ab7e24
Florin Mattmüller <florin.mattmueller@jacando.com>
2019-09-27 10:08:05 +0200

    Initial commit

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('./db/connect')
 
 const app = express()
 
-app.use(express.json())
+app.use(cors(bodyParser.json()))
+
 
 module.exports = app
