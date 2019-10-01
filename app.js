e049e4f0ac9e69abdb4a033a01d50cb19908f72b
Florin Mattmüller <florin.mattmueller@jacando.com>
2019-10-01 17:06:38 +0200

    =initial commit

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const { ApolloServer } = require('apollo-server-express')
 const bodyParser = require('body-parser')
 require('./db/connect')
 const helloWorldRouter = require('./routers/helloWorld')
+const typeDefs = require('./typeDefs/helloWorldDef')
+const resolvers = require('./resolvers/helloWorldResolver')
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express()
+server.applyMiddleware({ app });
 
 app.use(bodyParser.json())
 app.use('/api',helloWorldRouter)
-
 
 module.exports = app
