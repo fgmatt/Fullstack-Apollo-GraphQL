3f6a348dd4a8c8f36ff77b3fd188b59338aa9613
Florin Mattmüller <florin.mattmueller@jacando.com>
2019-10-28 12:56:53 +0100

    Added missing semicolon in index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const port = process.env.PORT;
 
 app.listen(port, () => {
-    console.log(`Server ready at http://localhost:${port}`)
+    console.log(`Server ready at http://localhost:${port}`);
 });
