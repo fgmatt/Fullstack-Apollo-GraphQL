a9ca59a4820ba836ac1989b686c228af763d09cf
Florin Mattmüller <florin.mattmueller@jacando.com>
2019-10-31 11:23:59 +0100

    GraphQl-Endpoint

--- src/graphQL/apollo-server.js
+++ src/graphQL/apollo-server.js
@@ -1,8 +1,12 @@
 const { ApolloServer } = require('apollo-server-express');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
-const { port } = require('../keys/keys')
+//const { port } = require('../keys/keys')
 
-const server = new ApolloServer({ typeDefs, resolvers, playground: { endpoint: `http://localhost:${port}/playground` } });
+const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    //playground: { endpoint: `http://localhost:${port}/playground`} 
+});
 
 module.exports = server;
