7d200acd933e8a11be74f95033b37a3e8c0b2c4e
Florin Mattmüller <florin.mattmueller@jacando.com>
2020-01-16 08:12:54 +0100

    mutations and queries

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,59 +1,47 @@
-import ApolloClient from 'apollo-boost';
-import { gql } from 'apollo-boost';
-import React from 'react';
-import { render } from 'react-dom';
-import { useQuery } from '@apollo/react-hooks';
+import React from "react";
+import { render } from "react-dom";
 
-import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
 const client = new ApolloClient({
-    uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  uri: "https://48p1r2roz4.sse.codesandbox.io"
 });
 
-client
-    .query({
-        query: gql`
-            {
-                rates(currency: "USD") {
-                    currency
-                }
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(gql`
+    {
+      rates(currency: "USD") {
+        currency
+        rate
+      }
+    }
+  `);
 
-            }
-        `    
-    })
-    .then(result => console.log(result));
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
 
 const App = () => (
-    <ApolloProvider client={client}>
-        <div>
-            <h2>My first Apollo app</h2>
-        </div>
-    </ApolloProvider>
+  <ApolloProvider client={client}>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <ExchangeRates />
+    </div>
+  </ApolloProvider>
 );
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
 
-const EXCHANGE_RATES = gql`
-    {
-        rates(currency: "USD") {
-            currency
-            rate
-        }
-    }
-`;
 
-function ExchangeRates() {
-    const { loading, error, data } = useQuery(EXCHANGE_RATES);
 
-    if(loading) return <p>Loading...</p>;
-    if(error) return <p>Error :(</p>;
-
-    return data.rates.map(({ currency, rate }) => (
-        <div key={currency}>
-            <p>
-                {currency}: {rate}
-            </p>
-        </div>
-    ));
-}
 
