30de1c75bad9fa6759361b513f0aa834b3f66333
Florin Mattmüller <florin.mattmueller@jacando.com>
2020-07-16 16:24:23 +0200

    remove unessesary code

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,6 @@ import cors from "cors";
 //import "./db/connect";
 import helloWorldRouter from "./routers/helloWorld";
 import server from "./graphQL/apollo-server";
-//import "./services/user";
 import "./jwt/jwt";
 
 const app = express();
