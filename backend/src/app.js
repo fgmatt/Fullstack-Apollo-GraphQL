37bec92528e8d15dc65a3919cf8304e875ed5918
Florin Mattmüller <florin.mattmueller@jacando.com>
2020-11-20 16:40:27 +0100

    add some additional security libraries

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import helmet from "helmet";
+import hpp from "hpp";
+import csurf from "csurf";
+import rateLimit from "express-rate-limit";
 //import "./db/connect";
 import helloWorldRouter from "./routers/helloWorld";
 import server from "./graphQL/apollo-server";
@@ -9,8 +13,17 @@ import "./jwt/jwt";
 const app = express();
 server.applyMiddleware({ app });
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+});
+
 app.use(bodyParser.json());
 app.use(cors());
+app.use(helmet());
+app.use(hpp());
+app.use(csurf());
+app.use(limiter);
 app.use("/api", helloWorldRouter);
 
 export default app;
